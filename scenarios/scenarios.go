d32616fb130138de70bcee4e2d16f6519316d10b
pk910 <philipp@pk910.de>
2023-11-06 07:26:20 +0100

    added blobless `eoatx` & `deploytx` scenarios

--- scenarios/scenarios.go
+++ scenarios/scenarios.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/ethpandaops/blob-spammer/scenarios/combined"
 	"github.com/ethpandaops/blob-spammer/scenarios/conflicting"
+	"github.com/ethpandaops/blob-spammer/scenarios/deploytx"
+	"github.com/ethpandaops/blob-spammer/scenarios/eoatx"
 	"github.com/ethpandaops/blob-spammer/scenarios/normal"
 	"github.com/ethpandaops/blob-spammer/scenarios/replacements"
 	"github.com/ethpandaops/blob-spammer/scenarios/wallets"
@@ -15,6 +17,9 @@ var Scenarios map[string]func() scenariotypes.Scenario = map[string]func() scena
 	"conflicting":  conflicting.NewScenario,
 	"normal":       normal.NewScenario,
 	"replacements": replacements.NewScenario,
+
+	"eoatx":    eoatx.NewScenario,
+	"deploytx": deploytx.NewScenario,
 
 	"wallets": wallets.NewScenario,
 }
